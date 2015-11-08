288047abc7eb60406314fdb8c20eef74ae00ca24
Alexandre <herole.alexandre@gmail.com>
2015-11-08 21:04:19 +0100

    Correction migration

--- db/migrate/20151108171406_add_file_name_to_correct_list.rb
+++ db/migrate/20151108171406_add_file_name_to_correct_list.rb
@@ -1,6 +1,5 @@
 class AddFileNameToCorrectList < ActiveRecord::Migration
   def change
-    remove_column :correct_lists, :file_name
     add_column :correct_lists, :file_name_id, :integer
   end
 end
